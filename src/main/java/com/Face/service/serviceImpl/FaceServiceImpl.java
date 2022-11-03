eae05d448822b96d4f82be1b1d7241cfe9dbcf9a
wucp <2696982657@qq.com>
2022-11-03 18:03:58 +0900

    修正

--- src/main/java/com/Face/service/serviceImpl/FaceServiceImpl.java
+++ src/main/java/com/Face/service/serviceImpl/FaceServiceImpl.java
@@ -4,17 +4,28 @@ import com.Face.service.FaceService;
 import com.baidu.aip.face.AipFace;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
+
 
 import javax.annotation.PostConstruct;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 
 @Service
+@PropertySource("config.properties")
 public class FaceServiceImpl implements FaceService {
+    @Value("${aipFace.appId}")
+    private String appId;
+    @Value("${aipFace.apiKey}")
+    private String apiKey;
+    @Value("${aipFace.secretKey}")
+    private String secretType;
+    @Value("${aipFace.imageType}")
+    private String imageType;
+    @Value("${aipFace.groupId}")
+    private String groupId;
     private HashMap<String, String> map = new HashMap<>();
     private AipFace aipFace;
 
@@ -25,12 +36,12 @@ public class FaceServiceImpl implements FaceService {
 
     @PostConstruct
     public void init() {
-        aipFace = new AipFace("28208907", "LGpL5SWmL01WRcMcOmvzXyDx", "VHmutq14cmEMw1qXK7OvdzlFPjwp6j7H");
+        aipFace = new AipFace(appId, apiKey, secretType);
     }
 
     @Override
     public String FaceRegister(String UserId, String image) {
-        JSONObject result = aipFace.addUser(image, "BASE64", "test", UserId, map);
+        JSONObject result = aipFace.addUser(image, imageType, groupId, UserId, map);
         System.out.println(result);
         Integer error_code = result.getInt("error_code");
         return error_code == 0 ? "成功" : "失败";
@@ -38,7 +49,7 @@ public class FaceServiceImpl implements FaceService {
 
     @Override
     public String FaceDetect(String image) {
-        JSONObject result = aipFace.detect(image, "BASE64", map);
+        JSONObject result = aipFace.detect(image, imageType, map);
         System.out.println(result);
         if (result.has("error_code") && result.getInt("error_code") == 0) {
             JSONObject jsonObject = result.getJSONObject("result");
@@ -51,7 +62,7 @@ public class FaceServiceImpl implements FaceService {
 
     @Override
     public String FaceLogin(String image) {
-        JSONObject searchResult = aipFace.search(image, "BASE64", "test", map);
+        JSONObject searchResult = aipFace.search(image, imageType, groupId, map);
         System.out.println(searchResult);
         if (searchResult.has("error_code") && searchResult.getInt("error_code") == 0) {
             JSONObject result = searchResult.getJSONObject("result");
@@ -60,7 +71,7 @@ public class FaceServiceImpl implements FaceService {
                 JSONObject user = userList.getJSONObject(0);
                 double score = user.getDouble("score");
                 if (score > 80) {
-                    return "成功"+" "+"用户ID为:"+user.getString("user_id");
+                    return "成功" + " " + "用户ID为:" + user.getString("user_id");
                 }
             }
         }
